4d5e64ef1937f0c1fde634cab3644708f2ad1140
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2023-04-02 00:03:04 -0300

    init project

--- index.mjs
+++ index.mjs
@@ -5,4 +5,17 @@ import chalk from "chalk";
 // internal modules
 import fs from "fs";
 
-console.log("iniciamos o accounts");
+operations()
+
+function operations() {
+  inquirer.prompt({
+    type: "list",
+    name: "action",
+    message: "O que você deseja fazer?",
+    choices: ["Criar conta", "Consultar saldo", "Depositar", "Sacar", "Sair"],
+  }).then((answer) => {
+    const action = answer['action'];
+
+    console.log(action);
+  }).catch(() => {});
+}
